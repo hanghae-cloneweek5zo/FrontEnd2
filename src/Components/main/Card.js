933b04958d07ca1c619c0d331e1d30e7318148df
SeoNaru <tjehddnr12345@naver.com>
2022-08-22 23:12:06 +0900

    pull

--- src/Components/main/Card.js
+++ src/Components/main/Card.js
@@ -2,77 +2,98 @@ import React, { Fragment } from 'react';
 import styled from 'styled-components';
 import { FaRegHeart } from 'react-icons/fa';
 // import { FaHeart } from 'react-icons/fa';
+import { AiFillStar } from 'react-icons/ai';
 
 const Card = () => {
   return (
     <Fragment>
-      <CardWrapper>
-        <CardItem>
-          <CardItemImg></CardItemImg>
-          <CardItemIcon>
-            <FaRegHeart style={{ color: '#fff' }} />
-            {/* <FaHeart style={{color: "#fff"}}/> */}
-          </CardItemIcon>
-          <p>Abiansemal, 인도네시아</p>
-          <p>5,275km</p>
-          <p>₩480,993 /박</p>
-        </CardItem>
-      </CardWrapper>
+      <CardContents>
+        <CardArea>
+          <CardImgArea
+            image_url={
+              'https://a0.muscache.com/im/pictures/e25a9b25-fa98-4160-bfd1-039287bf38b6.jpg?im_w=720'
+            }
+          >
+            <CardIconArea>
+              <FaRegHeart style={{ color: '#fff' }} />
+              {/* <FaHeart style={{color: "#fff"}}/> */}
+            </CardIconArea>
+          </CardImgArea>
+          <CardContentArea>
+            <div>
+              <span style={{ fontWeight: '600' }}>Abiansemal, 인도네시아</span>
+            </div>
+            <div>
+              <AiFillStar/>4.87
+            </div>
+          </CardContentArea>
+          <CardContentArea>
+            <div>
+              <span style={{ color: '#979a9e' }}>5,275km</span>
+            </div>
+          </CardContentArea>
+          <CardContentArea>
+            <div>₩480,993 / 박</div>
+          </CardContentArea>
+        </CardArea>
+      </CardContents>
     </Fragment>
   );
 };
 
 export default Card;
 
-export const CardWrapper = styled.div`
-  display: grid;
-  grid-auto-rows: minmax(0, 1fr);
-  grid-row-gap: 40px;
-  grid-column-gap: 24px;
+export const CardContents = styled.div`
+  display: grid !important;
+  grid-template-columns: 100%;
   isolation: isolate;
   box-sizing: border-box;
-  width: 100%;
-  margin-top: 25px;
-  grid-template-columns: repeat(5, minmax(100px, auto));
-  row-gap: 32px;
-  border: 1px solid red;
-  margin-block: 30px;
-  object-fit: cover !important;
-  vertical-align: bottom;
   &:hover {
     cursor: pointer;
   }
 `;
 
-export const CardItem = styled.div`
+export const CardArea = styled.div`
   grid-area: 1/1/2/2;
+  opacity: 1;
   position: relative;
-  display: block;
-  height: 400px;
-  border-radius: 15px;
-  border: 1px solid gray;
-  width: 100%;
-  top: 0;
-  left: 0;
 `;
 
-export const CardItemImg = styled.div`
-  display: block;
-  background-image: url(https://a0.muscache.com/im/pictures/e25a9b25-fa98-4160-bfd1-039287bf38b6.jpg?im_w=720);
-  background-size: cover;
+export const CardImgArea = styled.div`
+  background-image: url('${(props) => props.image_url}');
   background-position: center;
-  width: 100%;
-  height: 300px;
-  border-radius: 15px;
+  display: block;
+  background-size: cover;
+  width: 300px;
+  height: 280px;
+  border-radius: 12px;
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+  margin-bottom: 10px;
+  position: relative;
 `;
 
-export const CardItemIcon = styled.div`
+const CardContentArea = styled.div`
+  -webkit-box-pack: justify;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const CardIconArea = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 35px;
+  height: 35px;
   position: absolute;
-  right: 12px;
-  top: 12px;
+  top: 8px;
+  right: 8px;
+  color: #fff;
   svg {
     display: block;
-    height: 24px;
-    width: 24px;
+    height: 20px;
+    width: 20px;
+    stroke-width: 0.5;
+    overflow: visible;
   }
 `;
--- src/Components/main/Header.js
+++ src/Components/main/Header.js
@@ -9,7 +9,10 @@ import { useNavigate } from 'react-router-dom';
 import { FaAirbnb } from 'react-icons/fa';
 import { FaBars } from 'react-icons/fa';
 
-const Header = () => {
+import imgPath from '../../static/IconImages/profile.png';
+
+const Header = ({ isScrollTop, togglePopup, isLoggedIn, data }) => {
+  // const { profileImg } = data;
   const navigate = useNavigate();
 
   return (
@@ -21,14 +24,23 @@ const Header = () => {
             airbnb
           </HeaderLogoTitle>
         </HeaderLogo>
-        <HeaderPageButton  btnType="oval">
+        <HeaderPageButton
+          btnType="oval"
+          isScrollTop={isScrollTop}
+          onClick={togglePopup}
+        >
           <FaBars />
-          <HeaderProfileSvg style={{
-                fill: "#717171",
-                cursor: "pointer",
-                }}>
+          {/* {isLoggedIn && profileImg && <HeaderProfileImg src={profileImg} />}
+          {isLoggedIn && !profileImg && <HeaderProfileImg src={imgPath} />}
+          {!isLoggedIn && ( */}
+            <HeaderProfileSvg
+            style={{
+              fill: '#717171',
+              cursor: 'pointer',
+            }}>
             <path d="m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z"></path>
           </HeaderProfileSvg>
+          {/* )} */}
         </HeaderPageButton>
       </HeaderBox>
     </Fragment>
@@ -38,30 +50,26 @@ const Header = () => {
 export default Header;
 
 export const HeaderBox = styled.header`
-  position: sticky;
+  position: fixed;
+  width: 100%;
+  height: 80px;
   display: flex;
   justify-content: space-between;
-  margin: auto;
-  width: 100%;
-  max-width: 1600px;
-  height: 70px;
-  z-index: 1;
-  border-bottom: 1px solid #717171;
-  margin-bottom: 25px;
-  top: 0;
-  padding: 10px;
-  padding-left: 20px;
+  z-index: 100;
+  border-bottom: 1px solid rgb(235, 235, 235);
+  margin: -3px;
   box-sizing: border-box;
   flex-direction: row;
   align-items: center;
-  text-decoration: inherit;
+  padding: 20px 80px 15px 80px;
 `;
 
 export const HeaderLogo = styled.div`
-  height: 40px;
+  height: 80px;
+  width: 100%;
+  padding-left: 80px;
   display: flex;
   box-sizing: border-box;
-  justify-content: center;
   align-items: center;
   & svg {
     width: 35px;
@@ -87,21 +95,32 @@ export const HeaderPageButton = styled.div`
   height: 42px;
   box-sizing: border-box;
   align-items: center;
+  justify-content: center;
   border: 1px solid #ddd;
   display: flex;
   border-radius: 21px !important;
   padding: 5px 5px 5px 12px;
-  margin-right: 5px;
+  margin-right: 80px;
   background: transparent;
   box-shadow: 0 1px 2px rgb(0 0 0 / 8%), 0 4px 12px rgb(0 0 0 / 5%);
-  &svg{
+  &svg {
     background-color: #222222;
   }
-
+  &:hover {
+    transition: box-shadow 0.2s ease;
+    box-shadow: 2px 2px 4px 0px rgba(168, 168, 168, 0.5);
+  }
 `;
 
 export const HeaderProfileSvg = styled.svg`
   width: 32px;
   height: 32px;
   margin-left: 8px;
+`;
+
+export const HeaderProfileImg = styled.img`
+  width: 32px;
+  height: 32px;
+  margin-left: 8px;
+  border-radius: 50%;
 `;
